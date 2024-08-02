75ff39e9b651561cb80a55c39ced3a41a9264f4e
BenGeordie <school@bengeordie@gmail.com>
2024-08-02 15:39:36 -0400

    removed unnecessary env var

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,7 +15,6 @@ const nextConfig = {
   env: {
     DEPLOYMENT_BASE_URL: process.env.DEPLOYMENT_BASE_URL,
     THIRDAI_PLATFORM_BASE_URL: process.env.THIRDAI_PLATFORM_BASE_URL,
-    NDB_FRONTEND: process.env.NDB_FRONTEND,
   }
 };
 
