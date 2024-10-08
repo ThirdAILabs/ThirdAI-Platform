7e8eeb6cb43cf90c19438bb44b8f2c9ffdea8b34
Nicholas Meisburger <nmeisburger@gmail.com>
2024-10-08 16:25:07 -0400

    format

--- thirdai_platform/database/alembic/versions/7750a526cecc_new_workflow_api.py
+++ thirdai_platform/database/alembic/versions/7750a526cecc_new_workflow_api.py
@@ -7,8 +7,9 @@ Create Date: 2024-10-08 10:02:33.774774
 """
 
 from typing import Sequence, Union
-from alembic import op
+
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
