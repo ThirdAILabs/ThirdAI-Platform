0f1a291efcb46fc6782aced6bbd185430bac5d83
Nicholas Meisburger <nmeisburger@gmail.com>
2024-10-14 10:04:30 -0400

    fix path issue

--- integration_tests/utils.py
+++ integration_tests/utils.py
@@ -3,5 +3,6 @@ import os
 
 def doc_dir():
     return os.path.join(
-        os.path.dirname(os.path.dirname(__file__)), "train_job/sample_docs/"
+        os.path.dirname(os.path.dirname(__file__)),
+        "thirdai_platform/train_job/sample_docs/",
     )
